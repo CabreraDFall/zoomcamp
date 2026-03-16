0046aadcc2c7c3267b91445b134770853f09abed
Abraham Cabrera <testingdfall@gmail.com>
2026-03-16 15:09:51 +0100

    feat: Initialize Python data pipeline project with Docker, uv dependency management, and a script for data processing to parquet.

--- 01-docker-sql/pipeline/pipeline.py
+++ 01-docker-sql/pipeline/pipeline.py
@@ -1,3 +1,7 @@
 import sys
+import pandas as pd
 
-print("Hello from pipeline")
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
